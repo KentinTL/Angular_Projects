b52de132574b28f6ab9c68825bce7c32ce53d29b
quentin <q.thibaut.lazaro96@gmail.com>
2019-03-28 16:36:02 +0100

    myFirstApp

---
+++ src/app/user-profile/user-profile.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-profile',
+  templateUrl: './user-profile.component.html',
+  styleUrls: ['./user-profile.component.sass']
+})
+export class UserProfileComponent implements OnInit {
+  hidden: boolean; //false de base
+
+  User = {
+    photo : 'https://randomuser.me/api/portraits/lego/2.jpg',
+    name : 'Doe',
+    firstName : 'John',
+    quote : '',
+    age : 25,
+  };
+
+  afficherMasquerAge() {
+    this.hidden = !this.hidden;
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
